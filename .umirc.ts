2afa7ea107df7ac5f11efc5721df4b6a87fa521f
frllk <1402805300@qq.com>
2020-08-08 16:56:00 +0800

    lesson1：创建页面、配置路由

--- .umirc.ts
+++ .umirc.ts
@@ -6,5 +6,9 @@ export default defineConfig({
   },
   routes: [
     { path: '/', component: '@/pages/index' },
+    { path: '/cart', component: '@/pages/cart' },
+    { path: '/olist', component: '@/pages/olist' },
+    { path: '/user', component: '@/pages/user' },
+    { path: '/login', component: '@/pages/login' },
   ],
 });
